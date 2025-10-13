99c7850f2c52ebcbf6f1aa4e946183459fae4cd2
Egor Goncharov <forskype1919@gmail.com>
2025-10-13 23:45:23 +0300

    Added min window size

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using ElectronNET.API;
+using ElectronNET.API.Entities;
 using ThirdTry.Pages;
 
 namespace ThirdTry
@@ -58,7 +59,32 @@ namespace ThirdTry
 
         private async void CreateWindow()
         {
-            var window = await Electron.WindowManager.CreateWindowAsync();
+            var window = await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
+            {
+                // Текущий размер окна
+                Width = 1200,
+                Height = 800,
+                
+                // ⭐ ОГРАНИЧЕНИЯ МАСШТАБИРОВАНИЯ ⭐
+                MinWidth = 600,     // Минимальная ширина
+                MinHeight = 400,    // Минимальная высота  
+                MaxWidth = 1920,    // Максимальная ширина
+                MaxHeight = 1080,   // Максимальная высота
+                
+                // Дополнительные настройки поведения
+                Resizable = true,       // Разрешить изменение размера
+                Maximizable = true,     // Разрешить максимизацию
+                Minimizable = true,     // Разрешить минимизацию
+                Fullscreenable = true, // Запретить полноэкранный режим
+                
+                // Центрирование окна
+                Center = true,
+                
+                // Другие настройки (опционально)
+                Show = true,           // Не показывать сразу
+                Title = "ThirdTry"      // Заголовок окна
+            });
+    
             window.SetAutoHideMenuBar(true);
             window.OnClosed += () =>
             {
